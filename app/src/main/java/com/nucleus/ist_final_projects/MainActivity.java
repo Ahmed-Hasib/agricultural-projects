bd1077b3c76f43ffa1838f9fc3bab0d4f67fc1a2
hasibahmed999 <hasibahmed999@gmail.com>
2019-07-17 07:07:22 +0600

    ist final project starts

---
+++ app/src/main/java/com/nucleus/ist_final_projects/MainActivity.java
@@ -0,0 +1,40 @@
+package com.nucleus.ist_final_projects;
+
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Show_camera show_camera;
+    Camera camera;
+    FrameLayout frameLayout;
+    Button btn_capture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameLayout=(FrameLayout)findViewById(R.id.framelayout);
+        btn_capture=(Button)findViewById(R.id.btn_capture);
+        btn_capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"Captured",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //open the camera
+        camera=Camera.open();
+        show_camera=new Show_camera(this,camera);
+
+
+
+
+        frameLayout.addView(show_camera);
+    }
+}
